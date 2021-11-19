ccf9a72eb7d5039f130ae3475079be6365068e82
vonblubba <oscar.riva@gmail.com>
2021-11-19 18:03:22 +0100

    implement Generation#neighbours method and specs

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -24,4 +24,47 @@ class Generation < ApplicationRecord
   validates :iteration, presence: true
   validates :world, presence: true
   validates :iteration, uniqueness: { scope: :world }
+
+  def next_gen_state(x_coordinate:, y_coordinate:)
+    check_bounds(x_coordinate: x_coordinate, y_coordinate: y_coordinate)
+
+    current_state = grid[x_coordinate][y_coordinate]
+
+    # TODO: implement this
+  end
+
+  def neighbours(x_coordinate:, y_coordinate:)
+    check_bounds(x_coordinate: x_coordinate, y_coordinate: y_coordinate)
+
+    # brutal but effective :-)
+    result = []
+
+    neighs = [
+      [x_coordinate - 1, y_coordinate - 1],
+      [x_coordinate - 1, y_coordinate],
+      [x_coordinate - 1, y_coordinate + 1],
+      [x_coordinate, y_coordinate - 1],
+      [x_coordinate, y_coordinate + 1],
+      [x_coordinate + 1, y_coordinate - 1],
+      [x_coordinate + 1, y_coordinate],
+      [x_coordinate + 1, y_coordinate + 1]
+    ]
+
+    neighs.each do |n|
+      result << grid[n[0]][n[1]] if in_bounds?(x_coordinate: n[0], y_coordinate: n[1])
+    end
+
+    result
+  end
+
+  private
+
+  def in_bounds?(x_coordinate:, y_coordinate:)
+    x_coordinate >= 0 && x_coordinate <= grid.count && y_coordinate >= 0 && y_coordinate <= grid[0].count
+  end
+
+  def check_bounds(x_coordinate:, y_coordinate:)
+    raise 'x_coordinate out of bounds' unless x_coordinate >= 0 && x_coordinate <= grid.count
+    raise 'y_coordinate out of bounds' unless y_coordinate >= 0 && y_coordinate <= grid[0]&.count
+  end
 end
--- spec/models/generation_spec.rb
+++ spec/models/generation_spec.rb
@@ -56,4 +56,63 @@ RSpec.describe Generation, type: :model do
       end
     end
   end
+
+  context 'methods' do
+    let(:generation1) do
+      column1 = [0, 0, 0, 0]
+      column2 = [0, 0, 0, 0]
+      column3 = [0, 0, 0, 0]
+      column4 = [0, 0, 1, 0]
+      column5 = [0, 1, 1, 0]
+      column6 = [0, 0, 0, 0]
+      column7 = [0, 0, 0, 0]
+      column8 = [0, 0, 0, 0]
+
+      create(:generation, world: 'test_world1', iteration: 0, grid: [column1, column2, column3, column4, column5, column6, column7, column8])
+    end
+
+    let(:expected_generation2) do
+      column1 = [0, 0, 0, 0]
+      column2 = [0, 0, 0, 0]
+      column3 = [0, 0, 0, 0]
+      column4 = [0, 1, 1, 0]
+      column5 = [0, 1, 1, 0]
+      column6 = [0, 0, 0, 0]
+      column7 = [0, 0, 0, 0]
+      column8 = [0, 0, 0, 0]
+
+      create(:generation, world: 'test_world1', iteration: 1, grid: [column1, column2, column3, column4, column5, column6, column7, column8])
+    end
+
+    context '#next_gen_state' do
+      context 'provided out of bounds indexes' do
+        it 'raises error' do
+          expect { generation1.next_gen_state(x_coordinate: 0, y_coordinate: 100) }.to raise_error(RuntimeError)
+          expect { generation1.next_gen_state(x_coordinate: 100, y_coordinate: 0) }.to raise_error(RuntimeError)
+        end
+      end
+
+      it 'evaluates correctly next gen state' do
+        0.upto 7 do |x|
+          0.upto 3 do |y|
+            expect(generation1.next_gen_state(x_coordinate: x, y_coordinate: y)).to eq expected_generation2.grid[x][y]
+          end
+        end
+      end
+    end
+
+    context '#neighbours' do
+      context 'provided out of bounds indexes' do
+        it 'raises error' do
+          expect { generation1.neighbours(x_coordinate: 0, y_coordinate: 100) }.to raise_error(RuntimeError)
+          expect { generation1.neighbours(x_coordinate: 100, y_coordinate: 0) }.to raise_error(RuntimeError)
+        end
+      end
+
+      it 'returns neighbors array' do
+        expect(generation1.neighbours(x_coordinate: 0, y_coordinate: 0)).to eq [0, 0, 0]
+        expect(generation1.neighbours(x_coordinate: 4, y_coordinate: 1)).to eq [0, 0, 1, 0, 1, 0, 0, 0]
+      end
+    end
+  end
 end
